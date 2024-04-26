cdfa31be50dc29775375a84fee91fae8ef443e3c
NIdhi Singh <133571695+nidhisingh5859@users.noreply.github.com>
2024-04-26 16:13:35 +0530

    Corrected program1.js

--- program1.js
+++ program1.js
@@ -1,14 +1,22 @@
 var isValid = function(s) {
-  const stack = [];
-  const characters = { ')': '(', '}': '{', ']': '[' };
-  for (const char of s) {
-    if (!characters[char]) {
-      stack.push(char);
-    } else if (stack.pop() !== characters[char]) {
-      return false;
+    const stack = [];
+    const pairs = {
+        '(': ')',
+        '[': ']',
+        '{': '}'
+    };
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i];
+        if (pairs[char]) {
+            stack.push(char);
+        } else {
+            const lastOpening = stack.pop();
+            if (pairs[lastOpening] !== char) {
+                return false;
+            }
+        }
     }
-  }
-  return stack.length === 0;
+    return stack.length === 0;
 };
 
-module.exports = { isValid }
+module.exports = { isValid };
