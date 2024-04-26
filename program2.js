f14cef12f4333d42da95624e2e329161c8bf80f0
NIdhi Singh <133571695+nidhisingh5859@users.noreply.github.com>
2024-04-26 16:11:04 +0530

    Completed program2.js

--- program2.js
+++ program2.js
@@ -2,9 +2,32 @@
  * @param {string} s
  * @return {number}
  */
+/**
+ * @param {string} s
+ * @return {number}
+ */
 var romanToInt = function(s) {
-    
+    const romanValues = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+    let total = 0;
+    for (let i = 0; i < s.length; i++) {
+        const currentValue = romanValues[s[i]];
+        const nextValue = romanValues[s[i + 1]];
+
+        if (nextValue && currentValue < nextValue) {
+            total -= currentValue;
+        } else {
+            total += currentValue;
+        }
+    }
+    return total;
 };
 
-
-module.exports={romanToInt}
+module.exports = { romanToInt };
